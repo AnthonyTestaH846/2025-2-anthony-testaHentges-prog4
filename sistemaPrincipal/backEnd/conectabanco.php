e1d30ad98471911910526cfae239ba12c5520d67
Anthony Testa Hentges <anthonytestahentges@gmail.com>
2025-10-28 15:08:54 -0300

    organizaçao
    ConectaBanco nos phps

---
+++ sistemaPrincipal/backEnd/conectabanco.php
@@ -0,0 +1,12 @@
+<?php
+$username = 'root';
+$password = '';
+$dbname   = 'mabel_ptqa_anthony_eloiza_mateus';
+$host     = 'localhost';
+try {
+ $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch(PDOException $e) {
+    die('A conexão com o banco de dados falhou conectaBanco: ' . $e->getMessage());
+}
+?>
