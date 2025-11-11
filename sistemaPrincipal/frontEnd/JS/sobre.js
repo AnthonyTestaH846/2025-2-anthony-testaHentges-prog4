c13af44c1755c50d0097898da467f35c15dd4067
Anthony Testa Hentges <anthonytestahentges@gmail.com>
2025-11-11 15:14:53 -0300

    menu selecionado

---
+++ sistemaPrincipal/frontEnd/JS/sobre.js
@@ -0,0 +1,10 @@
+// Menu selecionado
+const menuItems = document.querySelectorAll('.menu-item');
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        menuItems.forEach(i => i.classList.remove('selected'));
+        
+        item.classList.add('selected');
+    });
+});
