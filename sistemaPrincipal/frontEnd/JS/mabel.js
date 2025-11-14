a6f159b354807576bcecbfa2b2b487f53cd226b0
Anthony Testa Hentges <anthonytestahentges@gmail.com>
2025-11-14 16:56:02 -0300

    Formulario Mabel
    JS HTML CSS

--- sistemaPrincipal/frontEnd/JS/mabel.js
+++ sistemaPrincipal/frontEnd/JS/mabel.js
@@ -1,48 +1,15 @@
-// Menu selecionado
-const menuItems = document.querySelectorAll('.menu-item');
+  //Formulário
+  document.getElementById("filterForm").addEventListener("submit", function(e) {
+    e.preventDefault();
 
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        menuItems.forEach(i => i.classList.remove('selected'));
-        
-        item.classList.add('selected');
-    });
-});
+    const start = new Date(document.getElementById("data_inicio").value);
+    const end = new Date(document.getElementById("data_fim").value);
+    const error = document.getElementById("errorMsg");
 
-const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-  const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
-
-  new Chart(document.getElementById("grafico01"), {
-    type: "line",
-    data: {
-      labels: xValues,
-      datasets: [{
-        label: "Exemplo",
-        fill: false,
-        tension: 0,
-        backgroundColor: "rgba(0,0,255,1)",
-        borderColor: "rgba(0,0,255,0.6)",
-        data: yValues
-      }]
-    },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          display: false
-        }
-      },
-      scales: {
-        y: {  // 👈 Agora é 'y', não 'yAxes'
-          min: 6,
-          max: 16
-        },
-        x: {  // 👈 idem para 'x'
-          title: {
-            display: true,
-            text: "Valores de X"
-          }
-        }
-      }
+    if (start > end) {
+      alert("A data inicial não pode ser maior que a data final.");
+      return
     }
-  });
+
+    error.textContent = "";
+});
