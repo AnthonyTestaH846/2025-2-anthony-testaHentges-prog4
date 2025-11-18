b3046eccd0a8852b8594d24b6e54b609e34ca69d
Mateus-Furlan-K <mateusfurlank2018@gmail.com>
2025-11-18 15:06:06 -0300

    r4
    u67r

--- sistemaPrincipal/backEnd/mabel/2.9mabel.php
+++ sistemaPrincipal/backEnd/mabel/2.9mabel.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json; charset=utf-8');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include 'conecta_mysql.php';
+include '../conecta_mysql.php';
 
 $dataInicial = $_GET['dataInicial'] ?? null;
 $dataFinal   = $_GET['dataFinal'] ?? null;
